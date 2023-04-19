02126eea09e0ac144ecb73ea65b92e067970930b
samquis <saquis503@gmail.com>
2023-04-18 23:27:06 -0400

    primer comit 001

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hola a todos, Soy <span className="purple">Samuel Quispe FLores </span>
+            de <span className="purple"> Potosí, Bolivia.</span>
+            <br />Soy un estudiante de septimo semestre que cursa materia de Seminario de Sistemas
+            de la Carrera de Ingenieria de Sistemas. 
+            <br />
+            <br />
+            Aparte de la codificacion, algunas otras actividades que me encanta hacer!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Jugar video juegos
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Escribir blogs de tecnologias
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Diseños
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">SamQuis</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/new-avatar.png";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              SOBRE <span className="purple"> MI </span> 
+            </h1>
+            <p className="home-about-body">
+              Soy un desarrollador junior apacionado por la programación y el aprendisaje constante,
+              Me encanta estar inmerso en la tecmnología y aprender nuevas habilidades y herramienteas 
+              de programación...🤷‍♂️
+              <br />
+              <br />Tengo conocimientos solidos en lenguajes de prgramacion
+              <i>
+                <b className="purple"> C++, Javascript and Python. </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b> and
+                also in areas related to{" "}
+                <b className="purple">
+                  Deep Learning and Natural Launguage Processing.
+                </b>
+              </i>
+              <br />
+              <br />
+              Siempre que puedo, tambien aplico por desarrollar productos con
+               <b className="purple"> Node.js</b> y
+              <i>
+                <b className="purple">
+                  {" "}
+                  Modern Javascript Library and Frameworks
+                </b>
+              </i>
+              &nbsp; como
+              <i>
+                <b className="purple"> React.js and Next.js</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/samquis"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/SamuelQuispeFl4"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/samuel-quispe-flores-20ab671b4/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
